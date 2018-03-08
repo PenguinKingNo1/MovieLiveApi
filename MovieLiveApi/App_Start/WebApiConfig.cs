8d116aee372aa3a600a467323741d853c85a39fa
zwang223@syr.edu <zwang@syr.edu>
2018-03-08 16:56:35 -0500

    Enable Cors

--- MovieLiveApi/App_Start/WebApiConfig.cs
+++ MovieLiveApi/App_Start/WebApiConfig.cs
@@ -5,6 +5,7 @@ using System.Net.Http;
 using System.Web.Http;
 using Microsoft.Owin.Security.OAuth;
 using Newtonsoft.Json.Serialization;
+using System.Web.Http.Cors;
 
 namespace MovieLiveApi
 {
@@ -25,6 +26,7 @@ namespace MovieLiveApi
                 routeTemplate: "api/{controller}/{id}",
                 defaults: new { id = RouteParameter.Optional }
             );
+            config.EnableCors();
         }
     }
 }
